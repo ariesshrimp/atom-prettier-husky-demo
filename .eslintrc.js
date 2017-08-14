015ceba072ad37f6227bac0c377bbc7b458ce179
joefraley <jose.fraley@gmail.com>
2017-08-14 13:04:54 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  extends: ["eslint:recommended", "prettier"],
+  plugins: ["babel", "prettier"],
+  parser: "babel-eslint",
+};
---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const x = 2
+var y = ''
+
+var somethinGelte
+let z = a =>
+b =>
+  x =>
+
+
+  function ({ ...all}) {
+  if (all) return new somethinGelte
+}
